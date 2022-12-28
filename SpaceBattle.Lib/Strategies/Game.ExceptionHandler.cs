48ee452de78b06117faf90aed7c88f317788c32d
IvanBugaenko <bugaenko1234567890@yandex.ru>
2022-12-28 21:46:46 +0600

    fix find

--- SpaceBattle.Lib/Strategies/Game.ExceptionHandler.cs
+++ SpaceBattle.Lib/Strategies/Game.ExceptionHandler.cs
@@ -6,18 +6,10 @@ public class FindExceptionHandlerStrategy : IStrategy
 {
     public object RunStrategy(params object[] args)
     {
-        var cmd = (Type)args[0];
-        var exception = (Type)args[1];
+        var list = (IEnumerable<Type>)args[0];
 
-        var tree = IoC.Resolve<IDictionary<object, IDictionary<object, IHandler>>>("Game.GetExceptionTree");
+        var tree = IoC.Resolve<IDictionary<int, IHandler>>("Game.GetExceptionTree");
 
-        if (!tree.TryGetValue(cmd, out IDictionary<object, IHandler>? dict))
-        {
-            var tree_exception = tree["default"];
-            if (!tree_exception.TryGetValue(exception, out IHandler? handler))
-                return tree_exception["default"];
-            return handler;
-        }
-        return (dict.TryGetValue(exception, out IHandler? handler1) ? handler1 : dict["default"]);
+        return tree.TryGetValue(IoC.Resolve<int>("Game.GetHashCode", list), out IHandler? value) ? value : tree[0];
     }
 }

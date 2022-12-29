85b6efd2c8468e1905d0cb6e5ca3c56f79a5793c
JulinwZ <julinwz@gmail.com>
2022-12-29 09:53:37 +0600

    9:53

---
+++ SpaceBattle.Lib/Strategies/Game.GetHashCodeStrategy.cs
@@ -0,0 +1,16 @@
+namespace SpaceBattle.Lib;
+
+public class GetHashCodeStrategy : IStrategy
+{
+    public object RunStrategy(params object[] args)
+    {
+        var list = (IEnumerable<object>)args[0];
+
+        unchecked
+        {
+            int hashcode = (int)232582681769;
+            list.Select(t => t.GetHashCode()).OrderBy(t => t).ToList().ForEach(n => hashcode = (hashcode * 7302013) ^ n);
+            return hashcode;
+        }
+    }
+}
